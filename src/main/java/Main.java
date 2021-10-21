e4b6956147d733a53bfcd06368a4c88b57b24c51
VershininaAlina <Kirillastrov@gmail.com>
2021-10-21 18:58:13 +0700

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Вывод всех целых чисел от 20 до 40: \n");
+        for (int i = 20; i <= 40; i++)
+        {
+            System.out.printf(" %d ",i );
+        }
+    }
+}
